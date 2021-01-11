5a02db740ee2661437457ae79229e6bd2a260c07
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-01-11 17:19:09 +0900

    click

--- nagomi/__init__.py
+++ nagomi/__init__.py
@@ -1,10 +1,21 @@
-import http.srever
+import http.server as s
+import click
 from oauthlib.oauth2 import RequestValidator, WebApplicationServer
 
 
 class MyRequestValidator(RequestValidator):
     pass
 
+
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+def server():
+    print('serve')
+
 def main():
     server = WebApplicationServer(MyRequestValidator())
     print(server)
+    cli()
